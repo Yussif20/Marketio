fc2d270d137cc7c4ca84cdd367dcdf642466a8bb
Yousef ayman <yousefayman2000500@gmail.com>
2024-11-05 17:22:48 +0200

    making the about slider responsive

--- src/components/Founders.jsx
+++ src/components/Founders.jsx
@@ -14,11 +14,27 @@ const Founders = () => {
       <Swiper
         modules={[Autoplay, Pagination]}
         spaceBetween={50}
-        slidesPerView={3}
         autoplay={{ delay: 5000, disableOnInteraction: false }}
         pagination={{ clickable: true }}
         allowTouchMove={true}
         className="w-full max-w-3xl"
+        breakpoints={{
+          // when window width is >= 640px
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          // when window width is >= 768px
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+          // when window width is >= 1024px
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
+        }}
       >
         {foundersData.map((founder, index) => (
           <SwiperSlide
