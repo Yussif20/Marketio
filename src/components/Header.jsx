08a96424ba7e6f9f01a4d876ebb6b92dd64dd80e
Yousef ayman <yousefayman2000500@gmail.com>
2024-11-06 12:51:31 +0200

    making the header responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,122 +5,159 @@ import { useState } from 'react';
 
 const Header = () => {
   const [showSearchBar, setShowSearchBar] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
+
   let linkStyles = `hover:font-semibold hover:underline transition-all duration-300`;
 
   const toggleSearchBar = () => {
     setShowSearchBar(!showSearchBar);
   };
 
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <header className="w-full border-b border-gray-300 py-4 dark:bg-darkSecondary dark:text-white">
-      <div className="flex items-center justify-evenly md:justify-between mx-2 sm:mx-4 md:mx-12 pb-2">
-        <h1 className="text-xl sm:text-2xl leading-6 font-bold">
+      <div className="flex items-center justify-evenly mx-2 sm:mx-4 md:mx-12">
+        <h1 className="text-xl sm:text-2xl font-bold">
           MAR<span className="text-basicRed">KET</span>IO
         </h1>
-        <nav>
-          <ul className="lg:flex text-base gap-10 hidden">
-            <li className={linkStyles}>
+
+        <div className="lg:hidden">
+          <button onClick={toggleMenu} className="text-2xl focus:outline-none">
+            <i
+              className={`fa-solid text-xl sm:text-2xl  ${
+                showMenu ? 'fa-times' : 'fa-bars'
+              }`}
+            ></i>
+          </button>
+        </div>
+
+        <nav className="hidden w-full xl:w-10/12 lg:flex justify-evenly items-center text-base list-none ">
+          <li className={linkStyles}>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? ' text-basicRed underline' : ''
+              }
+              to="/"
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className={linkStyles}>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? ' text-basicRed underline' : ''
+              }
+              to="/contact"
+            >
+              Contact
+            </NavLink>
+          </li>
+          <li className={linkStyles}>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? ' text-basicRed underline' : ''
+              }
+              to="/about"
+            >
+              About
+            </NavLink>
+          </li>
+          <li className={linkStyles}>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? ' text-basicRed underline' : ''
+              }
+              to="/sign"
+            >
+              Sign Up
+            </NavLink>
+          </li>
+        </nav>
+
+        <button className="block sm:hidden" onClick={toggleSearchBar}>
+          <i
+            className={`fa-solid fa-magnifying-glass text-xl sm:text-2xl  hover:text-basicRed ${
+              showSearchBar ? 'text-basicRed' : ''
+            }`}
+          ></i>
+        </button>
+
+        <div className="flex items-center justify-evenly gap-6">
+          <div className="hidden sm:block">
+            <SearchBar />
+          </div>
+          <div className="flex items-center gap-2">
+            <Link to="/favorite">
+              <i className="fa-regular fa-heart hover:text-basicRed text-xl sm:text-2xl transition-all duration-300"></i>
+            </Link>
+            <Link to="/cart">
+              <i className="fa-solid fa-cart-shopping hover:text-basicRed text-xl sm:text-2xl transition-all duration-300"></i>
+            </Link>
+            <button>
+              <i className="fa-regular fa-user hover:text-basicRed text-xl sm:text-2xl transition-all duration-300"></i>
+            </button>
+          </div>
+          <ThemeSwitcher />
+        </div>
+      </div>
+
+      {showSearchBar && (
+        <div className="sm:hidden flex items-center justify-center pt-2 sm:pt-0">
+          <SearchBar />
+        </div>
+      )}
+
+      {showMenu && (
+        <div className="lg:hidden">
+          <ul className="flex flex-col items-center gap-4 py-4  bg-gray-100 dark:bg-darkSecondary border-t dark:border-gray-700">
+            <li>
               <NavLink
                 className={({ isActive }) =>
-                  isActive ? ' text-basicRed underline' : ''
+                  `${linkStyles} ${isActive ? 'text-basicRed underline' : ''}`
                 }
                 to="/"
+                onClick={toggleMenu}
               >
                 Home
               </NavLink>
             </li>
-            <li className={linkStyles}>
+            <li>
               <NavLink
                 className={({ isActive }) =>
-                  isActive ? ' text-basicRed underline' : ''
+                  `${linkStyles} ${isActive ? 'text-basicRed underline' : ''}`
                 }
                 to="/contact"
+                onClick={toggleMenu}
               >
                 Contact
               </NavLink>
             </li>
-            <li className={linkStyles}>
+            <li>
               <NavLink
                 className={({ isActive }) =>
-                  isActive ? ' text-basicRed underline' : ''
+                  `${linkStyles} ${isActive ? 'text-basicRed underline' : ''}`
                 }
                 to="/about"
+                onClick={toggleMenu}
               >
                 About
               </NavLink>
             </li>
-            <li className={linkStyles}>
+            <li>
               <NavLink
                 className={({ isActive }) =>
-                  isActive ? ' text-basicRed underline' : ''
+                  `${linkStyles} ${isActive ? 'text-basicRed underline' : ''}`
                 }
                 to="/sign"
+                onClick={toggleMenu}
               >
                 Sign Up
               </NavLink>
             </li>
           </ul>
-        </nav>
-        <div className="flex items-center justify-center gap-3 md:gap-6 ">
-          <button className="block sm:hidden" onClick={toggleSearchBar}>
-            {' '}
-            <i
-              className={`fa-solid fa-magnifying-glass hover:text-basicRed ${
-                showSearchBar ? 'text-basicRed' : ''
-              }`}
-            ></i>
-          </button>
-
-          <div className="hidden sm:block">
-            <SearchBar />
-          </div>
-          <Link to="/favorite">
-            <i className="fa-regular fa-heart hover:text-basicRed text-2xl transition-all duration-300"></i>
-          </Link>
-          <Link to="/cart">
-            <svg
-              width="32"
-              height="32"
-              viewBox="0 0 32 32"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              className="stroke-current text-black dark:text-white"
-            >
-              <path
-                d="M11 27C11.5523 27 12 26.5523 12 26C12 25.4477 11.5523 25 11 25C10.4477 25 10 25.4477 10 26C10 26.5523 10.4477 27 11 27Z"
-                strokeWidth="1.5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M25 27C25.5523 27 26 26.5523 26 26C26 25.4477 25.5523 25 25 25C24.4477 25 24 25.4477 24 26C24 26.5523 24.4477 27 25 27Z"
-                strokeWidth="1.5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M3 5H7L10 22H26"
-                strokeWidth="1.5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M10 16.6667H25.59C25.7056 16.6667 25.8177 16.6267 25.9072 16.5535C25.9966 16.4802 26.0579 16.3782 26.0806 16.2648L27.8806 7.26479C27.8951 7.19222 27.8934 7.11733 27.8755 7.04552C27.8575 6.97371 27.8239 6.90678 27.7769 6.84956C27.73 6.79234 27.6709 6.74625 27.604 6.71462C27.5371 6.68299 27.464 6.66661 27.39 6.66666H8"
-                strokeWidth="1.5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-          </Link>
-          <button>
-            <i className="fa-regular fa-user"></i>
-          </button>
-          <ThemeSwitcher />
-        </div>
-      </div>
-      {showSearchBar && (
-        <div className="sm:hidden flex items-center justify-center pt-2">
-          <SearchBar />
         </div>
       )}
     </header>
