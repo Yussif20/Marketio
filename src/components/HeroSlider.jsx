16fcd2ebf7a1645eb718e6c4033cfceba417612b
Yousef ayman <yousefayman2000500@gmail.com>
2024-12-01 00:35:28 +0200

    fix the heroslider change lang direction bug

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -1,19 +1,22 @@
-// Import Swiper React components and required modules
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Pagination } from 'swiper/modules';
-
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
 import { useTranslation } from 'react-i18next';
+import { useProductContext } from '../ProductContext';
 
 const HeroSlider = () => {
   const { t } = useTranslation();
+  const { direction } = useProductContext();
 
   const heroData = t('hero.data', { returnObjects: true });
 
   return (
-    <div className="flex justify-center items-center my-12 max-w-[320px] sm:max-w-[550px] lg:max-w-[900px]">
+    <div
+      className={`flex justify-center items-center my-12 ${
+        direction === 'rtl' ? 'rtl-class' : ''
+      } max-w-[320px] sm:max-w-[550px] lg:max-w-[900px]`}
+    >
       <Swiper
         modules={[Autoplay, Pagination]}
         spaceBetween={50}
@@ -21,17 +24,25 @@ const HeroSlider = () => {
         autoplay={{ delay: 5000, disableOnInteraction: false }}
         pagination={{ clickable: true }}
         allowTouchMove={true}
+        dir={!direction ? 'rtl' : 'ltr'}
+        key={direction}
         className="w-full max-w-3xl"
       >
         {heroData.map((product, index) => (
           <SwiperSlide key={index}>
             <div className="relative">
               <img
-                className="w-full  max-h-[350px] object-cover"
+                className="w-full max-h-[350px] object-cover"
                 src={product.image}
                 alt={product.title}
               />
-              <div className="absolute z-10 text-white top-20 left-8 sm:top-16 ">
+              <div
+                className={`absolute z-10 text-white top-20 ${
+                  direction === 'rtl'
+                    ? 'right-8 sm:right-16'
+                    : 'left-8 sm:left-16'
+                }`}
+              >
                 <h3>{product.title}</h3>
                 <p className="text-2xl sm:text-5xl font-semibold md:my-4">
                   {t('hero.title')}
